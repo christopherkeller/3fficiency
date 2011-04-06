782f76504d2a3cbb4524ab3ca0965971e7f6145a
Jeff Gonzalez <likwid@gmail.com>
2011-04-05 22:51:07 -0700

    Changed dev_settings.py to example_settings.py and removed passwords from file.  Also added dev_settings.py to gitignore.

--- threefficiency/example_settings.py
+++ threefficiency/example_settings.py
@@ -1,5 +1,5 @@
 #These settings should be in a file called dev_settings.py in the web folder.
 DATABASE_NAME='nventio'
-DATABASE_USERNAME='ryans'
-DATABASE_PASSWORD='password'
+DATABASE_USERNAME='$user'
+DATABASE_PASSWORD='$password'
 DATABASE_HOST='127.0.0.1'
