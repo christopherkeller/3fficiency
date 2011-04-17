c9d75ec77ad8deda3857cd274c8c3c9e50f84e5f
Ryan Spaulding <ryanspaulding@gmail.com>
2011-04-17 00:06:38 -0700

    have basic stuff working, still need to style and figure out the slider thing

---
+++ static/js/status.js
@@ -0,0 +1,58 @@
+
+
+jQuery(document).ready(function() {
+	var group = '';
+	var time_frame = '';	
+
+	/* what gets fired off when a group button is pressed on the homepage (/) */
+	jQuery(".button").click(function() {
+		group = jQuery(this).val();
+		if (jQuery("#time_select").is(":visible")) {
+			var status_url = "/group/" + group + "/status/" + time_frame +  "/json/";
+			jQuery.get(status_url, function(data) {
+				build_status_list(data);
+			});
+		}
+		jQuery("#time_select").slideDown('slow');	
+	});
+
+	/* what gets fired off when the time frame drop down appears (after the button has been clicked */
+	jQuery("#time_frame").change(function() {
+		time_frame = jQuery(this).val();
+		var status_url = "/group/" + group + "/status/" + time_frame +  "/json/";
+		jQuery.get(status_url, function(data) {
+			build_status_list(data);
+		});
+	});
+});
+
+
+/* data looks like this:
+	data.keys = Array of keys
+	data[key] = Array of status
+*/
+function build_status_list(data) {
+	
+
+	if (data.keys.length == null) {
+		return;
+	}
+
+	html = '<ul>';
+	jQuery.each(data.keys, function(index) {
+		html += "<li>" +  data.keys[index];
+		html += "<ul>";
+		for (var i = 0; i < data[data.keys[index]].length; i++) {
+			// "Christopher Keller": [["03/29/2011", "yes i got that done"]]
+			// 0 == date and 1 == status
+			//html += "<li>" + data[index][i][0] + " " + data[index][i][1] + "</li>";
+			html += "<li>" + data[data.keys[index]][i][0] + " " + data[data.keys[index]][i][1] + "</li>";
+		}
+		html += "</ul>";	
+		html += "</li>";
+	});
+	html += '</ul>';
+	jQuery("#display_status").html(html).slideDown('slow');
+
+
+}
--- threefficiency/status/views.py
+++ threefficiency/status/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import logout
 from django.shortcuts import render_to_response
 from django.core import serializers
 from django.core.context_processors import csrf
+import json
+import datetime
 
 def status_detail(request, status_id):
     status_details = Status.objects.get(id=status_id)
@@ -64,3 +66,40 @@ def get_latest_status_by_user_tag(request, format):
 
 	if request.method == 'POST':
 		return HttpResponse(data, mimetype)
+
+def get_all_status_by_user(request, user_name, group_name, format):
+	if format == 'json':
+		mimetype = 'application/json'
+
+	data = serializers.serialize(format,
+		Status.objects.filter(
+			user__id=User.objects.filter(user_name=user_name)[0].id
+		).filter(
+			group__id=Group.objects.filter(group_name=group_name)[0].id
+		)) 
+
+	return HttpResponse(data, mimetype)
+
+def get_all_status_by_group(request, group_name, time_frame, format):
+	if format == 'json':
+		mimetype = 'application/json'
+
+	# for now time_frame will be in days (7, 23, etc)
+
+	today = datetime.date.today()
+	days_ago = today - datetime.timedelta(days=int(time_frame))
+
+	all_objects = {}
+	all_objects['keys'] = []
+
+	for s in Status.objects.filter(group__id=Group.objects.filter(group_name=group_name)[0].id).filter(date__gt=days_ago):
+		user = "%s %s" % (s.user.first_name, s.user.last_name)
+		if user in all_objects:
+			all_objects[user].append([s.date.strftime('%m/%d/%Y'), s.completed_status]) 
+		else:
+			all_objects[user] = []
+			all_objects[user].append([s.date.strftime('%m/%d/%Y'), s.completed_status]) 
+			all_objects['keys'].append(user)
+
+
+	return HttpResponse(json.dumps(all_objects), mimetype)
